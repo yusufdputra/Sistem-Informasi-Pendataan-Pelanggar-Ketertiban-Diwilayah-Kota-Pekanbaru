95edf62b00882225182ba42c602dc6584317491e
alceoritter <alceoritter@gmail.com>
2021-05-20 16:52:26 +0700

    fix layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="col-12">
-    <div class="row">
+  <div class="row">
 
-    <div class="col-lg-4 col-xs-12">
+    <div class="col-lg-12 col-xs-12">
       <div class="card-box">
-       
+
       </div>
 
     </div>
--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -1,28 +1,24 @@
 <!-- Top Bar Start -->
 <div class="topbar">
 
-<!-- LOGO -->
-<div class="topbar-left">
-    <a href="{{'/'}}" class="logo"><img src="{{asset('adminto/images/brand/logo.png')}}" width="50px" alt="">SIMLANTIB</span><i class="mdi mdi-layers"></i></a>
-</div>
+  <!-- LOGO -->
+  <div class="topbar-left">
+    <a href="{{'/'}}" class="logo"><img src="{{asset('adminto/images/brand/logo.png')}}" width="50px" alt=""><span>SIMLANTIB</span></a>
+  </div>
 
-<!-- Button mobile view to collapse sidebar menu -->
-<div class="navbar navbar-default" role="navigation">
+  <!-- Button mobile view to collapse sidebar menu -->
+  <div class="navbar navbar-default" role="navigation">
     <div class="container-fluid">
 
-        <!-- Page title -->
-        <ul class="nav navbar-nav list-inline navbar-left">
-            <li class="list-inline-item">
-                <button class="button-menu-mobile open-left">
-                    <i class="mdi mdi-menu"></i>
-                </button>
-            </li>
-            <li class="list-inline-item">
-                <h4 class="page-title">{{$title}}</h4>
-            </li>
-        </ul>
+      <!-- Page title -->
+      <ul class="nav navbar-nav list-inline navbar-left">
+      
+        <li class="list-inline-item">
+          <h4 class="page-title">{{$title}}</h4>
+        </li>
+      </ul>
 
     </div><!-- end container -->
-</div><!-- end navbar -->
+  </div><!-- end navbar -->
 </div>
-<!-- Top Bar End -->
+  <!-- Top Bar End -->
