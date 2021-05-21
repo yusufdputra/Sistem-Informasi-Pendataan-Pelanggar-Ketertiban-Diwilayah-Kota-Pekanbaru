1c7cdbc3338b2848da6a5923ffb8e8b7d5ce8f37
alceoritter <alceoritter@gmail.com>
2021-05-21 20:55:57 +0700

    edit home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="col-12">
-  <div class="row">
+<div class="row">
+  <div class="col-12">
+    <div class="card-box ">
 
-    <div class="col-lg-12 col-xs-12">
-      <div class="card-box">
+      <h2><strong>Satuan Polisi Pamong Praja Kota Pekanbaru</strong></h2>
+      <div class="col-lg-12 row">
+      <div class="col-lg-4 col-xs-12 right">
+        <img src="{{asset('adminto/images/big/bg2.jpg')}}" width="100%"  class="" alt="profile-image">
+      </div>
+      <div class="col-lg-8 col-xs-12">
+        <h4><strong>VISI</strong></h4>
+        <p>Terwujudnya Satuan Polisi Pamong Praja Kota Pekanbaru Yang Profesional Terdepan Dalam Penegakkan Perda Dan Perkada.</p>
+        <p>Untuk Mewujudkan Visi Satuan Polisi Pamong Praja Kota Pekanbaru Tersebut, Maka Diupayakan Perwujudannya Melalui :</p>
 
+        <h4><strong>MISI</strong></h4>
+        <ol>
+          <li>Meningkatkan Ketertiban Umum Dan Ketentraman Masyarakat.</li>
+          <li>Meningkatkan Kepatuhan Dan Ketaatan Terhadap Perundang-undangan Daerah.</li>
+          <li>Meningkatkan Kesiapsiagaan Dan Kemandirian Satlinmas.</li>
+          <li>Meningkatkan Kemampuan Dan Keterampilan Sumberdaya Aparatur Satpol PP.</li>
+          <li>Meningkatkan Kelancaran Dan Ketertiban Pelayanan Internal.</li>
+        </ol>
       </div>
 
-    </div>
+    
+      </div>
+     
 
+    </div>
   </div>
 </div>
 
